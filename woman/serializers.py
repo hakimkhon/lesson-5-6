692a814e2fb54cfc7d4b745d7a58e203fe95a0e2
hakimkhon <hakimkhon94@gmail.com>
2022-07-04 15:59:38 +0500

    lesson 5 the and

--- woman/serializers.py
+++ woman/serializers.py
@@ -1,18 +1,18 @@
-import json
 from rest_framework import serializers
 from .models import Woman
 from rest_framework.renderers import JSONRenderer
-
+import io
+from rest_framework.parsers import JSONParser
 # class WomanSerializers(serializers.ModelSerializer):
 #     class Meta:
 #         model = Woman
 #         fields = ('id', 'title', 'content', 'created', 'updated', 'published', 'category')
 #         # fields = ('title', 'category')
 
-class WomanModel:
-    def __init__(self, title, content):
-        self.title = title
-        self.content = content
+# class WomanModel:
+#     def __init__(self, title, content):
+#         self.title = title
+#         self.content = content
 
 class WomanSerializers(serializers.Serializer):
     id = serializers.CharField(read_only = True)
@@ -23,10 +23,19 @@ class WomanSerializers(serializers.Serializer):
     published = serializers.BooleanField(default=True)
     category_id = serializers.CharField()
 
+    def create(self, validated_date):
+        return Woman.objects.create(**validated_date)
 
 # def encode():
-#     model = WomanModel('Title', 'Content')
+#     model = WomanModel('Title', 'Content: content')
 #     model_sr = WomanSerializers(model)
 #     print(model_sr.data, type(model_sr.data), sep='\n')
 #     json = JSONRenderer().render(model_sr.data)
 #     print(json)
+
+# def decode():
+#     stream = io.BytesIO(b'{"title":"Title","content":"Content: content"}')
+#     data = JSONParser().parse(stream)
+#     serializer = WomanSerializers(data=data)
+#     serializer.is_valid()
+#     print(serializer.validated_data)
--- woman/views.py
+++ woman/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render
-from .models import Woman
 from .serializers import WomanSerializers
-from rest_framework.generics import ListAPIView, ListCreateAPIView, RetrieveUpdateAPIView, DestroyAPIView
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework.generics import ListAPIView, ListCreateAPIView, RetrieveUpdateAPIView, RetrieveDestroyAPIView
 from rest_framework import status
 from django.http.response import JsonResponse
 
+from .models import Woman
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from django.forms.models import model_to_dict
 
 
-class DetailWoman(RetrieveUpdateAPIView, DestroyAPIView):
+class DetailWoman(RetrieveUpdateAPIView, RetrieveDestroyAPIView):
     queryset = Woman.objects.all()
     serializer_class = WomanSerializers
 
@@ -18,20 +18,19 @@ class DetailWoman(RetrieveUpdateAPIView, DestroyAPIView):
 #     queryset = Woman.objects.all()
 #     serializer_class = WomanSerializers
 
-
-class WomanAPIView(APIView):
-
-    def get(self, request):
-        lst = Woman.objects.all()
-        return Response({'posts': WomanSerializers(lst, many=True).data})
 # class WomanAPIView(APIView):
 #     def get(self, request):
 #         lst = Woman.objects.all().values()
 #         return Response({'posts': list(lst)})
 
+class WomanAPIView(APIView):
 
+    def get(self, request):
+        lst = Woman.objects.all()
+        return Response({'posts': WomanSerializers(lst, many=True).data}) #many bu malumot bo'lsa uni to'yxat shaklida chiqarish uchun
+        
     def post(self, request):
-        print("post")
+        # print("post")
         # return Response({'title': 'hammaga salom'})
         serializer = WomanSerializers(data=request.data)    #hatolikni ko'rsatmaslik
         serializer.is_valid(raise_exception=True)
@@ -41,18 +40,18 @@ class WomanAPIView(APIView):
             content = request.data['content'],
             category_id = request.data['category_id']
         )
-
-        # return Response({'post':model_to_dict(post_new)})
         return Response({'post': WomanSerializers(post_new).data})
+        # return Response({'post':model_to_dict(post_new)})
+
 
     # def put(self, request):
     #     print("put ishladi")
 
-    def delete(self, request):
-        pk=request.data["id"]
-        woman = Woman.objects.get(pk=pk)
-        woman.delete()
-        return JsonResponse({"status": "deleted"}, status=status.HTTP_204_NO_CONTENT)
+    # def delete(self, request): 
+    #     pk=request.data["id"]
+    #     woman = Woman.objects.get(pk=pk)
+    #     woman.delete()
+    #     return JsonResponse({"status": "deleted"}, status=status.HTTP_204_NO_CONTENT)
 
 
 
